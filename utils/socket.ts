4de5539644ba74986c516885747f5741e8b25915
Shirley de Jong <shirleydejong@users.noreply.github.com>
2026-03-07 15:34:35 +0100

    Update socket.ts

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,10 +1,14 @@
 import { io, Socket } from 'socket.io-client';
+import config from '../config';
 
 let socket: Socket | null = null;
 
 export function getSocket(): Socket {
   if (!socket) {
-    socket = io('http://localhost:3001', {
+    const socketURL = new URL(window.location);
+    socketURL.port = config.SOCKET_PORT.toString();
+    
+    socket = io(socketURL.toString(), {
       transports: ['websocket', 'polling'],
       reconnection: true,
       reconnectionDelay: 1000,
